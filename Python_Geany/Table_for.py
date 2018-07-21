45451261f1d1a63c54f1d0d3226e724bda45d7f5
DengYangWu <40263710+DengYangWu@users.noreply.github.com>
2018-07-21 20:33:31 +0800

    table——for

--- Python_Geany/Table_for.py
+++ Python_Geany/Table_for.py
@@ -1,5 +1,24 @@
 magicians = ['alice','david','carolina']
 for magician in magicians:
 	print(magician)
+	
+magicians = ['alice','david','carolina']
+for m in magicians:
+	print(m.title() + "\tthat was a great trick!")
+print("Thank you,everyone.That was a great magic show!")
+
+pizza = ['ChinaPizza','ItalyPizza','FrancePizza','AmericaPizza'] #I like pizza
+for p in pizza:
+	print(p)
+
+for p in pizza:
+	print(p.title() + "\tI like pepperoni pizza")
+
+print("I really love pizza")
+
+animal = ['cat','dog','tiger','leopard']
+for an in animal:
+	print("A "+ an.title() +" would make g great pet")
+print("animals would make a great pet!")
 
 
